8061f22dd9bd3579f2887bbf10816dd76527aa38
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-11-15 15:22:43 +0100

    ADD: todos essential functions

--- js/main.js
+++ js/main.js
@@ -2,6 +2,28 @@ Vue.config.ignoredElements = [/^ion-/];
 
 new Vue({
 	el: "#root",
-	data: {},
-	methods: {},
+	data: {
+		todos: [
+			{ text: "Creare una web app", completed: false },
+			{ text: "Fare la spesa", completed: true },
+			{ text: "Imparare Vue.js", completed: false },
+		],
+		inputValue: "",
+	},
+	methods: {
+		removeTodo(index) {
+			this.todos.splice(index, 1);
+			console.log(index);
+		},
+		clearInput() {
+			this.inputValue = "";
+		},
+		addFocus() {
+			this.$refs.inputField.focus();
+		},
+		addTodo() {
+			if (this.inputValue.trim() !== "") this.todos.push({ text: this.inputValue, completed: false });
+			this.clearInput();
+		},
+	},
 });
